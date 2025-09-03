053b7abf722940a7fabf653321bd28c4807c30a9
Ori Donner <smartech.devops.test@gmail.com>
2025-09-03 23:01:26 +0300

    fix bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "file-upload-terraform-state-a67aa6ad " 
+    bucket = "file-upload-terraform-state-a67aa6ad"
     key    = "terraform.tfstate"
     region = "eu-north-1"    
   }
